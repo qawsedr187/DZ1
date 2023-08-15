68b62107f90bd9bda06e3354ce4ce2060f69f138
qawsedr187 <121182353+qawsedr187@users.noreply.github.com>
2023-08-15 11:41:34 +0300

    Update lesson_1_task_7.py

--- lesson_1_task_7.py
+++ lesson_1_task_7.py
@@ -1,40 +1,41 @@
 def num_1():
-    print(1)
+    print(1, end='')
 
 def num_2():
-    print(2)
+    print(2, end='')
 
 def num_3():
-    print(3)
+    print(3, end='')
 
 def num_4():
-    print(4)
+    print(4, end='')
 
 def num_5():
-    print(5)
+    print(5, end='')
 
 def num_6():
-    print(6)
+    print(6, end='')
 
 def num_7():
-    print(7)
+    print(7, end='')
 
 def num_8():
     print(8, end='')
-    print(8, end='')
-    print(0, end='')
-    print(0, end='')
-    print(5, end='')
-    print(5, end='')
-    print(5, end='')
-    print(3, end='')
-    print(5, end='')
-    print(3, end='')
-    print(5, end='')
+
 
 def num_9():
-    print(9)
+    print(9, end='')
 
 def num_0():
-    print(0)
+    print(0, end='')
 num_8()
+num_8()
+num_0()
+num_0()
+num_5()
+num_5()
+num_5()
+num_3()
+num_5()
+num_3()
+num_5()
